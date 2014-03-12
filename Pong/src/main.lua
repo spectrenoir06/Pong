48789ca91c32e69ac15f419d07df77458c587731
spectrenoir06 <antoine.doussaud@laposte.net>
2014-03-12 23:06:10 +0100

    init

---
+++ Pong/src/main.lua
@@ -0,0 +1,15 @@
+Gamestate = require "class/Gamestate"
+Camera    = require "class/Camera"
+Timer     = require "class/Timer"
+
+function love.load()
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  
+end
